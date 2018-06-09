118de139f959ae65b34d70acb8a2e1ea738daf33
FlashXT <SXXT2015@outlook.com>
2018-06-09 19:04:01 +0800

    IPython,FlashXT,2018.6.9,Saturday

--- PythonDataAnalysis/Test_repr.py
+++ PythonDataAnalysis/Test_repr.py
@@ -2,9 +2,19 @@
 class Message:
 	def __init__(self,msg):
 		self.msg = msg
-	# def __repr__(self):
-	# 	return "Message : %s" % self.msg
+
+		return 1
+
+	def __repr__(self):
+		return "Message : %s" % self.msg
 
 x = Message("Ipython NoteBook")
 print(x)
 print(x.msg)
+
+x = 5
+y = 7
+if x > 5:
+	x+=1
+
+	y=8
